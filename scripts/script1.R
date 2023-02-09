9335550b478ddda3878c027003a94fb9f86f6504
cyrilfm <cyril.favre-martinoz@insee.fr>
2023-02-09 12:54:22 +0000

    creation du projet

---
+++ scripts/script1.R
@@ -0,0 +1,8 @@
+#chargement du package stat
+
+#création d'un vecteur allant de 1 à 100
+vec<-1:100
+summary(vec)
+
+hist(vec)
+
---
+++ scripts/script2.R
@@ -0,0 +1,5 @@
+#generation d'une loi normale
+
+norm<-rnorm(10000000,0,1)
+quantile(norm)
+hist(norm)
